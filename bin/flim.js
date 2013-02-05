fd1c64da4b55f9fd6972561830b1e53377820fc4
George Ornbo <george@shapeshed.com>
2013-02-05 07:21:11 +0000

    work in progress

--- bin/flim.js
+++ bin/flim.js
@@ -2,20 +2,31 @@
 var rimraf = require('rimraf'),
   fs = require('fs'),
   parser = require('../lib/parser'),
+  ArchivePage = require('../lib/generators/archive'),
+  IndexPage = require('../lib/generators/index'),
+  PostsPages = require('../lib/generators/posts'),
   Generator = require('../lib/generator');
 
-var arguments = process.argv.splice(2);
+var args = process.argv.splice(2);
 
-if (arguments[0] === "generate") {
+if (args[0] === "generate") {
   rimraf('./build', function (err) {
     fs.mkdir('./build', function (err, mkdir) {
       if (err) { throw err; }
       parser.init('./src/_posts/', function (err, data) {
-        var generator = new Generator(data);
-        generator.init();
+        //var generator = new Generator(data);
+        //generator.init();
+        var archive = new ArchivePage(data);
+        var index = new IndexPage(data.posts[0]);
+        var postsPages = new PostsPages(data);
+        archive.init(function(err, data){
+        });
+        index.init(function(err, data){
+        });
+        postsPages.init(function(err, data){
+        });
       });
-    });
-  });
+    }); });
 }
 
 
