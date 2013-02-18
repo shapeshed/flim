1a931cbf53498cd6cd0856f4c9a2019ed55f4cee
George Ornbo <george@shapeshed.com>
2013-02-18 10:00:01 +0000

    move npm test to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,4 +29,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
+  grunt.registerTask('build', 'simplemocha');
+
 };
