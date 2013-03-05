c675df8308736ddf49fbeed172b824053a5255f2
George Ornbo <george@shapeshed.com>
2013-03-05 22:16:34 +0000

    move generation to generate command

--- bin/flim.js
+++ bin/flim.js
@@ -2,6 +2,8 @@
 var flim = require("../lib/flim"),
   args = process.argv.slice(2);
 
-flim.init(function(err, data) {
-  if (err) { throw err; }
-});
+if (args[0] === "generate") {
+  flim.init(function(err, data) {
+    if (err) { throw err; }
+  });
+}
