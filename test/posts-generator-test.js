86a9f083130e29340fec2056bfd365696444c435
George Ornbo <george@shapeshed.com>
2013-03-05 22:17:45 +0000

    pass siteData and config to posts

--- test/posts-generator-test.js
+++ test/posts-generator-test.js
@@ -2,24 +2,23 @@ var assert = require("assert"),
   factories = require('./support/factories'),
   rimraf = require('rimraf'),
   fs = require('fs'),
+  path = require('path'),
+  config = require(path.resolve(__dirname + '/../lib/templates/flimrc.json')),
   PostsGenerator = require('../lib/generators/posts');
 
 describe('PostsGenerator', function () {
 
   beforeEach(function(done) {
-    fs.mkdir('./tmp', function (err, data) {
+    fs.mkdir('./build', function (err, data) {
       done();
     });
   });
 
   it('should create a folder for the first post', function (done) {
-    var options = {
-      template: "./test/fixtures/_layouts/post.jade",
-      destination: "./tmp/"
-    };
-    var postsGenerator = new PostsGenerator(factories.posts, options);
+    var postsGenerator = new PostsGenerator(factories, config);
     postsGenerator.init(function (err, data) {
-      fs.stat(options.destination + 'factory-post', function(err, data) {
+      if (err) { throw err; }
+      fs.stat(config.build_dir + '/' + 'factory-post', function(err, data) {
         if (err) { throw err; }
         assert.deepEqual(data.isDirectory(), true);
         done();
@@ -28,13 +27,9 @@ describe('PostsGenerator', function () {
   });
 
   it('should write an index.html file within the first post folder', function (done) {
-    var options = {
-      template: "./test/fixtures/_layouts/post.jade",
-      destination: "./tmp/"
-    };
-    var postsGenerator = new PostsGenerator(factories.posts, options);
+    var postsGenerator = new PostsGenerator(factories, config);
     postsGenerator.init(function (err, data) {
-      fs.exists(options.destination + 'factory-post/index.html', function(exists) {
+      fs.exists(config.build_dir + '/' + 'factory-post/index.html', function(exists) {
         assert.deepEqual(exists, true);
         done();
       });
@@ -42,13 +37,9 @@ describe('PostsGenerator', function () {
   });
 
   it('should populate the index file with the expected contents', function (done) {
-    var options = {
-      template: "./test/fixtures/_layouts/post.jade",
-      destination: "./tmp/"
-    };
-    var postsGenerator = new PostsGenerator(factories.posts, options);
+    var postsGenerator = new PostsGenerator(factories, config);
     postsGenerator.init(function (err, data) {
-      fs.readFile(options.destination + 'factory-post/index.html', 'utf-8', function(err, data) {
+      fs.readFile(config.build_dir + '/' + 'factory-post/index.html', 'utf-8', function(err, data) {
         if(err) { throw err; }
         assert.deepEqual(data, '<!DOCTYPE html><html><head><title>Factory post</title></head><body><h1>Factory post</h1><h1>Some HTML!</h1></body></html>');
         done();
@@ -57,13 +48,9 @@ describe('PostsGenerator', function () {
   });
 
   it('should create a folder for the second post', function (done) {
-    var options = {
-      template: "./test/fixtures/_layouts/post.jade",
-      destination: "./tmp/"
-    };
-    var postsGenerator = new PostsGenerator(factories.posts, options);
+    var postsGenerator = new PostsGenerator(factories, config);
     postsGenerator.init(function (err, data) {
-      fs.stat(options.destination + 'factory-post2', function(err, data) {
+      fs.stat(config.build_dir + '/' + 'factory-post2', function(err, data) {
         if (err) { throw err; }
         assert.deepEqual(data.isDirectory(), true);
         done();
@@ -72,13 +59,9 @@ describe('PostsGenerator', function () {
   });
 
   it('should write an index.html file within the second post folder', function (done) {
-    var options = {
-      template: "./test/fixtures/_layouts/post.jade",
-      destination: "./tmp/"
-    };
-    var postsGenerator = new PostsGenerator(factories.posts, options);
+    var postsGenerator = new PostsGenerator(factories, config);
     postsGenerator.init(function (err, data) {
-      fs.exists(options.destination + 'factory-post2/index.html', function(exists) {
+      fs.exists(config.build_dir + '/' + 'factory-post2/index.html', function(exists) {
         assert.deepEqual(exists, true);
         done();
       });
@@ -86,13 +69,9 @@ describe('PostsGenerator', function () {
   });
 
   it('should populate the index file with the expected contents', function (done) {
-    var options = {
-      template: "./test/fixtures/_layouts/post.jade",
-      destination: "./tmp/"
-    };
-    var postsGenerator = new PostsGenerator(factories.posts, options);
+    var postsGenerator = new PostsGenerator(factories, config);
     postsGenerator.init(function (err, data) {
-      fs.readFile(options.destination + 'factory-post2/index.html', 'utf-8', function(err, data) {
+      fs.readFile(config.build_dir + '/' + 'factory-post2/index.html', 'utf-8', function(err, data) {
         if(err) { throw err; }
         assert.deepEqual(data, '<!DOCTYPE html><html><head><title>Factory post 2</title></head><body><h1>Factory post 2</h1><h1>Some more HTML!</h1></body></html>');
         done();
@@ -101,7 +80,7 @@ describe('PostsGenerator', function () {
   });
 
   afterEach(function(done) {
-    rimraf('./tmp', function (err) {
+    rimraf('./build', function (err) {
       done();
     });
   });
